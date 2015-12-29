99a90da5dde741f074118d550a28ce1e8cbd8626
axlbert <aclanger131@gmail.com>
2015-12-29 17:19:18 +0100

    Search Function fully implemented
    now using visible function of marker,
    debugging the database with var vm = ko.dataFor(document.body);  and
    then vm.ArrayIamAfter()  was quite useful to locate variables and to
    debug constructor method (confusion of self and this)

--- bower_components/ownJS/map.js
+++ bower_components/ownJS/map.js
@@ -1,83 +1,183 @@
-$(document).ready(function () {
-   ko.applyBindings(new ViewModel);
-});
+/** legacy database to be modified, for now just includes some coords and names as titles for reuse*/
 
- var iniLocations = [
-  {
-      clickCount : 0,
-      name: "Kitty",
-      imgSrc: "img/catpick.jpg",
-      imgAttribution: "0",
-      namelist : ["snoopy","poopy", "whoopy"]
-    }, {
-      clickCount : 0,
-      name: "Patty",
-      imgSrc: "img/catpick2.jpg",
-      imgAttribution: "0",
-      namelist : ["woopy","goopy", "sloopy"]
-    }, {
-      clickCount : 0,
-      name: "Polly",
-      imgSrc: "img/catpick3.jpg",
-      imgAttribution: "0",
-      namelist : ["pappy","poopy", "whoopy"]
-    }, {
-      clickCount : 0,
-      name: "Molly",
-      imgSrc: "img/catpick4.jpg",
-      imgAttribution: "0",
-      namelist : ["pappy", "whoopy"]
-    }, {
-      clickCount : 0,
-      name: "Mappy",
-      imgSrc: "img/catpick5.jpg",
-      imgAttribution: "0",
-      namelist : ["frappy"]
-  }];
+var iniLocations = [{
+  clickCount: 0,
+  name: "Best Restaurant",
+  descr: "awesome food",
+  coords: {
+    lat: -7.100,
+    lng: 140.044
+  },
+  namelist: ["snoopy", "poopy", "whoopy"]
+}, {
+  clickCount: 0,
+  name: "Best Pub",
+  descr: "awesome beer",
+  coords: {
+    lat: 5.210,
+    lng: 101.044
+  },
+  namelist: ["woopy", "goopy", "sloopy"]
+}, {
+  clickCount: 0,
+  name: "Landmark",
+  descr: "awesome building",
+  coords: {
+    lat: -17.164,
+    lng: 135.044
+  },
+  namelist: ["pappy", "poopy", "whoopy"]
+}, {
+  clickCount: 0,
+  name: "Viewpoint",
+  descr: "awesome view",
+  coords: {
+    lat: -8.640,
+    lng: 115.044
+  },
+  namelist: ["pappy", "whoopy"]
+}, {
+  clickCount: 0,
+  name: "Furniture Store",
+  descr: "awesome chairs",
+  coords: {
+    lat: 12.930,
+    lng: 111.044
+  },
+  namelist: ["frappy"]
+}];
 
-
-
-var Loc = function(data) {
-  this.clickCount = ko.observable(data.clickCount);
+/**supposed to empty the sidebar with the names as the function is hit and then repopulate it with items that match the keyboard input supplied by query.*/
+var Constructor = function(data){
   this.name = ko.observable(data.name);
-  this.imgSrc = ko.observable(data.imgSrc);
-  this.imgAttribution = ko.observable(data.imgAttribution);
-  this.namelist = ko.observableArray(data.namelist);
+  this.coords = ko.observable(data.coords);
 
+  //self.searchList = ko.observableArray('');
+  //self.searchList = nameList.slice(0);
 };
 
 
 var ViewModel = function() {
+  /**launching the google map*/
     var self = this;
-    self.myMap = ko.observable({
-        lat: ko.observable(50),
-        lng: ko.observable(5)});
+    self.map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: 5.210,lng: 101.044},
+    scrollwheel: false,
+    zoom: 4
+  });
 
-    this.locationList = ko.observableArray([]);  // storing the cats, but before
+/** create an array for all markers that will be observed for changes*/
+  self.markerArray = ko.observableArray();
 
-    iniLocations.forEach(function(locItem){  //create a "new Cat" out of each item
-      self.locationList.push( new Loc(locItem) );
-    });
+/** constructor function for the markers, did not work outside the ViewModel though*/
+  self.constructMarkers = function(itemNo) {
+    /** confusing self and this here have been a fantastic error source*/
+  this.marker = new google.maps.Marker({
+    position: new google.maps.LatLng(itemNo.coords.lat, itemNo.coords.lng),
+    map: self.map,
+    title: itemNo.descr
+    })
+};
 
-    this.currentLoc = ko.observable(this.locationList()[0] );
 
-    this.setLoc = function(clickedLoc) { //setcat has been paired with each text item in the list
-      self.currentLoc(clickedLoc);  //we dont click on the name, we actually click on the object
+ iniLocations.forEach(function(itemNo){
+ self.markerArray.push(new self.constructMarkers(itemNo))
+});
+
+/**throws all newly created markes into the markerArray for further modification */
+ //  self.buildMarkerArray = function(val) {
+ // iniLocations.forEach(function(val){
+ // self.markerArray.push(new self.constructMarkers(val));
+ // });
+ // };
+
+
+  /**"smart" version of iniLocations where each array item has been "constructed" with observable features*/
+  /** should not be manipulated for the search function, is just ready to display things*/
+  /** should also not be used to display the ever changing filtermenu-list*/
+  self.locList = ko.observableArray([]);
+
+  /** smartifying all the array items and pushing them into the observable array above*/
+  /** should not be manipulated for the search function either*/
+  /** should also not be used to display the ever changing filtermenu-list*/
+  iniLocations.forEach(function(locItem){
+    self.locList.push(new Constructor(locItem));
+  });
+
+/** currently selected array, should be all locations by default*/
+/** this should receive a different scope when search is hit*/
+/**only contains one item right now*/
+/** not suitable for use of menufilter-list, only containts one item and thus one name*/
+  self.currentSearch = ko.observable(this.locList()[0]);
+
+ /** Keep this for later when selecting an item from the list, BUT additional search function is needed */
+  self.setLoc = function(filtLoc){
+    self.currentSearch(filtLoc);
   };
+
+  /**should be used to push the current search result in and then render the menu from it*/
+  /** problem is to not have it empty at first */
+  self.filterList = ko.observableArray([]);
+
+  /** populating a copy of the locationlist with the dataset for better filtering*/
+ self.buildFilterList = function(val) {
+  iniLocations.forEach(function(val){
+    self.filterList.push(val);
+  });
+ };
+
+  self.buildFilterList();
+//  self.buildMarkerArray();
+  // self.markerArray.removeAll()
+
+  /** stores the users input*/
+  self.input = ko.observable('');
+
+  self.hideMarkers = function(){
+    var len = iniLocations.length;
+    for (var i=0;i < len; i++) {
+    self.markerArray()[i].marker.setVisible(false);
+  }
+};
+
+  self.showMarkers = function(val){
+    self.markerArray()[val].marker.setVisible(true);
+  }
+
+self.showAllMarkers = function(){
+    var len = iniLocations.length;
+    for (var i=0;i < len; i++) {
+    self.markerArray()[i].marker.setVisible(true);
+  }
+};
+
+  /** result of this should populate the filterList, according to search input*/
+  self.filterFunc = function(){
+    self.filterList.removeAll();
+    self.hideMarkers();
+
+    var filterInput = self.input().toLowerCase();
+    if (filterInput.length > 1) {
+          //console.log("filter function is hit");
+    iniLocations.forEach(function(val){
+      //self.markerArray.push(new self.constructMarkers(val));
+    if (val.name.toLowerCase().indexOf(filterInput)>= 0) {
+
+      self.showMarkers(iniLocations.indexOf(val));
+      //console.log("if clause of filter function is hit");
+      self.filterList.push(val);
+            }})
+     } else { this.buildFilterList();
+              self.showAllMarkers(); };
+
+   };
+
+
 };
 
 
-ko.bindingHandlers.map = {
 
-    init: function (element, valueAccessor, allBindingsAccessor, viewModel) {
-        var mapObj = ko.utils.unwrapObservable(valueAccessor());
-        var latLng = new google.maps.LatLng(
-            ko.utils.unwrapObservable(mapObj.lat),
-            ko.utils.unwrapObservable(mapObj.lng));
-        var mapOptions = { center: latLng,
-                          zoom: 5,
-                          mapTypeId: google.maps.MapTypeId.ROADMAP};
+/**knockout.js go-live*/
+ko.applyBindings(new ViewModel());
 
-        mapObj.googleMap = new google.maps.Map(element, mapOptions);
-    }
-};
+var foo = ViewModel.markerArray;
